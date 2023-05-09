92d88a601c1e28d97804a36767b0d92f664ca255
Aike van den Brink <aikevandenbrink@gmail.com>
2023-05-09 11:48:18 +0200

    Automated commit.

--- Router.tsx
+++ Router.tsx
@@ -2,38 +2,19 @@ import React from 'react';
 import {View} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {Map} from './Map';
 
-const optionsHideHeader = {
-  headerShown: false,
-} as const;
-
 const BottomTabNavigator = createBottomTabNavigator();
-
-function BottomTab() {
-  return (
-    <BottomTabNavigator.Navigator>
-      <BottomTabNavigator.Screen name={'A'} component={Map} />
-      <BottomTabNavigator.Screen name={'B'} component={View} />
-      <BottomTabNavigator.Screen name={'C'} component={View} />
-    </BottomTabNavigator.Navigator>
-  );
-}
-
-const RootStack = createNativeStackNavigator();
 
 export function Router() {
   return (
     <NavigationContainer>
-      <RootStack.Navigator initialRouteName={'BottomTab'}>
-        <RootStack.Screen
-          component={BottomTab}
-          name={'BottomTab'}
-          options={optionsHideHeader}
-        />
-      </RootStack.Navigator>
+      <BottomTabNavigator.Navigator>
+        <BottomTabNavigator.Screen name={'A'} component={Map} />
+        <BottomTabNavigator.Screen name={'B'} component={View} />
+        <BottomTabNavigator.Screen name={'C'} component={View} />
+      </BottomTabNavigator.Navigator>
     </NavigationContainer>
   );
 }
